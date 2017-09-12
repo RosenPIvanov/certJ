28417042c08c095d47f03649897513e2337f7bfe
Rosen Ivanov <rosen.p.ivanov@gmail.com>
2017-09-12 21:34:39 +0300

    Test

---
+++ src/main/java/ocp/jdbc/JDBCTest.java
@@ -0,0 +1,21 @@
+package ocp.jdbc;
+
+import java.sql.*;
+
+/**
+ * Created by dia on 12.9.2017 г..
+ */
+public class JDBCTest {
+    public static void main(String args[]) throws SQLException {
+       //use Drivermanager Connection conn = new Connection(url, userName, password);
+        String url = "";
+        String userName = "";
+        String password = "";
+        Connection conn = DriverManager.getConnection(url, userName, password);
+
+        Statement stmt = conn.createStatement();
+         ResultSet rs = stmt.executeQuery("select count(*) from animal");
+         if (rs.next()) System.out.println(rs.getInt(1));
+
+    }
+    }
